af56402a352d6a1902f9b8da00c75f487c540091
ujwalkarippalichandran <ujwlkpl@gmail.com>
2025-04-16 21:11:34 -0600

    Added google sign in

--- src/firebase.js
+++ src/firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Auth setup
 const auth = getAuth(app);
